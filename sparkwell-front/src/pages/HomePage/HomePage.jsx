e83eeaafe68bf25a04841a4c1d04a914340601d3
Simrit Sidhu <simmysidhu@simrits-mbp.home>
2024-11-23 21:16:37 -0500

    Add front-end correspondence with the back-end

--- sparkwell-front/src/pages/HomePage/HomePage.jsx
+++ sparkwell-front/src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,7 @@
 import "../../App.scss";
 import "./HomePage.scss";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
-import { useParams, useNavigate } from "react-router-dom";
 
 const baseUrl = import.meta.env.VITE_BASE_URL;
 
@@ -43,13 +42,20 @@ const MoodSearch = ({ onMoodSelect }) => {
 // HomePage Component
 function HomePage() {
   const [selectedMood, setSelectedMood] = useState("");
+  const [playlists, setPlaylists] = useState([]);
 
-  const handleMoodSelect = (mood) => {
+  const handleMoodSelect = async (mood) => {
     setSelectedMood(mood);
     console.log(`Selected mood: ${mood}`);
-    // Can make an API call or take action with the selected mood
-  };
 
+    try {
+      //Sending the mood to the back-end
+      const response = await axios.get(`${baseUrl}/mood-playlist`, { mood });
+      setPlaylists(response.data.playlists);
+    } catch (error) {
+      console.error("Error getting the playlist:", error);
+    }
+  };
   return (
     <div className="mood">
       <p className="mood__reflect">Let's reflect on your mood.</p>
@@ -57,22 +63,34 @@ function HomePage() {
 
       <MoodSearch onMoodSelect={handleMoodSelect} />
 
-      <button className="mood__button">Generate Mood-Based Song</button>
-
-      <div className="journal">
-        <p className="journal__entry">
-          Journal about your day. Reflect on the best parts of your day and
-          release what doesn't serve you.
-        </p>
-        <input
-          className="journal__input"
-          name="journal__input"
-          placeholder="Let's reflect."
-        />
-        <button className="journal__button">Reflect</button>
+      <div className="playlist">
+        <h3>Playlist for "{selectedMood}" mood:</h3>
+        <ul>
+          {playlists.map((playlist, index) => (
+            <li key={index}>
+              <a href={playlist.url} target="_blank" rel="noopener noreferrer">
+                {playlist.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <button className="mood__button">Generate Mood-Based Song</button>
       </div>
     </div>
   );
 }
+
+<div className="journal">
+  <p className="journal__entry">
+    Journal about your day. Reflect on the best parts of your day and release
+    what doesn't serve you.
+  </p>
+  <input
+    className="journal__input"
+    name="journal__input"
+    placeholder="Let's reflect."
+  />
+  <button className="journal__button">Reflect</button>
+</div>;
 
 export default HomePage;
