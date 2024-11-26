847c651ba1a9a9afd8934b9548ac5d92d1544379
Simrit Sidhu <simmysidhu@mac.home>
2024-11-26 14:53:23 -0500

    Change input to textarea, create form, and add functionality to the submit button to submit to the journal entries

--- sparkwell-front/src/components/Journal/Journal.jsx
+++ sparkwell-front/src/components/Journal/Journal.jsx
@@ -1,7 +1,27 @@
 import "./Journal.scss";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
-const Journal = () => {
+const Journal = ({ onNewEntry }) => {
+  const [journalEntry, setJournalEntry] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/journalentries",
+        { entry: journalEntry }
+      );
+    } catch (error) {
+      alert("Failed to save your reflection. Please try again.");
+    }
+
+    //Clear after submission
+    setJournalEntry("");
+    alert("Your reflection has been saved into the Journal Entries tab!");
+  };
+
   return (
     <div className="journal">
       <p className="journal__entry">
@@ -9,15 +29,20 @@ const Journal = () => {
         release what doesn't serve you.
       </p>
 
-      {/* add a form */}
-      <input
-        className="journal__input"
-        name="journal__input"
-        placeholder="Let's reflect."
-      />
-      <button className="journal__button" type="submit">
-        Reflect
-      </button>
+      <form className="journal__form" onSubmit={handleSubmit}>
+        <label htmlFor="journal__input" className="journal__label"></label>
+        <textarea
+          id="journal__input"
+          className="journal__input"
+          name="journal__input"
+          placeholder="Let's reflect."
+          value={journalEntry}
+          onChange={(e) => setJournalEntry(e.target.value)}
+        />
+        <button className="journal__button" type="submit">
+          Reflect
+        </button>
+      </form>
     </div>
   );
 };
