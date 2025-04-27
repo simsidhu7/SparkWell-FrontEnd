3cd82f222f5a8f52c368467e49faa6be87cfd1fd
Simrit Sidhu <simmysidhu@mac.home>
2025-04-26 21:18:27 -0400

    Revert back to JSON from SQL

--- sparkwell-front/src/pages/JournalEntries/JournalEntries.jsx
+++ sparkwell-front/src/pages/JournalEntries/JournalEntries.jsx
@@ -25,12 +25,8 @@ function JournalEntries() {
       {journalEntries.length > 0 ? (
         journalEntries.map((entry) => (
           <div key={entry.id} className="journal-entry">
-            <p className="journal-entry__content">
-              Entry: {entry.JournalEntry}
-            </p>
-            <span className="journal-entry__date">
-              Date: {entry.TimeStamp.split("T")[0]}
-            </span>
+            <p className="journal-entry__content">Entry: {entry.entry}</p>
+            <span className="journal-entry__date">Date: {entry.timestamp}</span>
             <div className="journal-entry__divider"></div>
           </div>
         ))
